3d1d0dc568b5bcd6669b856aeb80415c54cab736
João Victor <jv191207@gmail.com>
2025-06-23 01:18:14 -0300

    funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -1,14 +1,15 @@
 #include "funcoes.h"
 
 char defconceito(int nota) {
-    if (nota >= 90)
+    if (nota >= 90) {
         return 'A';
-    else if (nota >= 80)
+    } else if (nota >= 80) {
         return 'B';
-    else if (nota >= 70)
+    } else if (nota >= 70) {
         return 'C';
-    else if (nota >= 60)
+    } else if (nota >= 60) {
         return 'D';
-    else
+    } else {
         return 'F';
+    }
 }
